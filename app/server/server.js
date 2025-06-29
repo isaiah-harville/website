b734cc7ff827b874c49e55317a12d853deaf4ce2
IsaiahHarvi <harvillerisaiah@gmail.com>
2025-06-29 18:00:19 -0500

    cors stuff

--- app/server/server.js
+++ app/server/server.js
@@ -8,12 +8,27 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
 // Middleware
 app.use(helmet());
+const whitelist = [
+  'https://isaiah.harville.dev',
+  'http://localhost:3000'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
